0299e1da901fc6c9cd125e7ca894a26bf098be57
taylord109 <cisco.cat@hotmail.com>
2019-04-10 17:12:31 -0400

    New Comment

--- src/app/monster-parser/monster-parser.component.ts
+++ src/app/monster-parser/monster-parser.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, ViewChild, OnChanges, ElementRef } from '@angular/core';
+import { clipboard } from 'electron';
 
 @Component({
   selector: 'pf-monster-parser',
@@ -21,7 +22,11 @@ export class MonsterParserComponent implements OnInit, OnChanges {
   public Hello() {
     const monster: string = this.monsterInput.nativeElement.value;
     // let d = new Fuse()
+
     console.log(monster);
+  }
+  public de(){
+    clipboard.readText('selection');
   }
 
 }
